d773e4e8edcef77b953f7622573d327a9f22ff8a
drexqq <01037923530a@gmail.com>
2022-08-25 00:13:11 +0900

    init : settings 1. svelte, vite, tailwindcss, autoprefix

---
+++ app/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from './components/Header.svelte';
+	import Hero from './components/Test.svelte';
+	import Team from './components/Team.svelte';
+	import Service from './components/Service.svelte';
+	import Footer from './components/Footer.svelte';
+</script>
+
+<main class="max-w mx-auto px-4">
+<div class="pt-4 pb-12">
+	<Header />
+	<Hero />
+	<Service />
+	<Team />
+	<Footer />
+	</div>
+</main>
